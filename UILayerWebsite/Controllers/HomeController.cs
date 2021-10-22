7125e5b6bcd48b335b286fe3b299eca9537ecb9f
the_nza <the_nza@protonmail.com>
2021-10-22 12:44:21 +0300

    Extract solver logic into separate controller and move views. Also add some UI fixes (dropdown menu, etc)

--- UILayerWebsite/Controllers/HomeController.cs
+++ UILayerWebsite/Controllers/HomeController.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Linq;
-using BusinessLayer;
-using DbLayer;
-using DbLayer.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using UILayerWebsite.Models;
@@ -13,15 +9,10 @@ namespace UILayerWebsite.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
-		private readonly Db _db;
 
 		public HomeController(ILogger<HomeController> logger)
 		{
 			_logger = logger;
-
-			// Init database
-			_db = new Db(new StockRepository(Helpers.DefaultStocksDbPath),
-				new PortfolioRepository(Helpers.DefaultPortfolioDbPath));
 		}
 
 		public IActionResult Index()
@@ -39,94 +30,6 @@ namespace UILayerWebsite.Controllers
 		{
 			return View(
 				new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-		}
-
-		public IActionResult First(string owner, string date)
-		{
-			FirstModel model = new FirstModel();
-
-			try
-			{
-				if (!String.IsNullOrWhiteSpace(owner) && !String.IsNullOrWhiteSpace(date))
-				{
-					DateTime parsedDate = DateTime.Parse(date);
-					Solver slvr = new Solver(_db);
-
-					model.Result = slvr.SolveOne(owner, parsedDate);
-					model.Owner = owner;
-					model.Date = parsedDate;
-					model.IsRes = true;
-				}
-			}
-			catch (NullReferenceException e)
-			{
-				_logger.LogError(e.ToString());
-				model.IsError = true;
-				model.ErrMessage = "Выбранная дата отсутствует";
-			}
-			catch (Exception e)
-			{
-				_logger.LogError(e.ToString());
-				model.IsError = true;
-				model.ErrMessage = "Внутренняя ошибка";
-			}
-
-			model.Portfolios = _db.PortfolioRepository.GetAll();
-			return View(model);
-		}
-
-		public IActionResult Second(string[] stockNames)
-		{
-			SecondModel model = new SecondModel();
-			try
-			{
-				if (stockNames.Length > 0)
-				{
-					Solver slvr = new Solver(_db);
-					model.Result = slvr.SolveTwo(stockNames.ToList());
-					model.IsRes = true;
-				}
-			}
-			catch (Exception e)
-			{
-				_logger.LogError(e.ToString());
-				model.IsError = true;
-				model.ErrMessage = "Внутренняя ошибка";
-			}
-
-			model.Stocks = _db.StockRepository.GetAll();
-			return View(model);
-		}
-
-		public IActionResult Third(string owner, string dateFrom, string dateTo)
-		{
-			ThirdModel model = new ThirdModel();
-
-			try
-			{
-				if (!String.IsNullOrWhiteSpace(owner) && !String.IsNullOrWhiteSpace(dateFrom) &&
-				    !String.IsNullOrWhiteSpace(dateTo))
-				{
-					DateTime dFrom = DateTime.Parse(dateFrom);
-					DateTime dTo = DateTime.Parse(dateTo);
-					Solver slvr = new Solver(_db);
-
-					model.Result = slvr.SolveThree(owner, dFrom, dTo);
-					model.Owner = owner;
-					model.DateFrom = dFrom;
-					model.DateTo = dTo;
-					model.IsRes = true;
-				}
-			}
-			catch (Exception e)
-			{
-				_logger.LogError(e.ToString());
-				model.IsError = true;
-				model.ErrMessage = "Внутренняя ошибка";
-			}
-
-			model.Portfolios = _db.PortfolioRepository.GetAll();
-			return View(model);
 		}
 	}
 }
---
+++ UILayerWebsite/Controllers/SolverController.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Linq;
+using BusinessLayer;
+using DbLayer;
+using DbLayer.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UILayerWebsite.Models;
+
+namespace UILayerWebsite.Controllers
+{
+	public class SolverController : Controller
+	{
+		private readonly ILogger<SolverController> _logger;
+		private readonly Db _db;
+
+		public SolverController(ILogger<SolverController> logger)
+		{
+			_logger = logger;
+
+			// Init db
+			_db = new Db(
+				new StockRepository(Helpers.DefaultStocksDbPath),
+				new PortfolioRepository(Helpers.DefaultPortfolioDbPath)
+			);
+		}
+
+		// First assignment solver
+		public IActionResult First(string owner, string date)
+		{
+			FirstModel model = new FirstModel();
+
+			try
+			{
+				if (!String.IsNullOrWhiteSpace(owner) && !String.IsNullOrWhiteSpace(date))
+				{
+					DateTime parsedDate = DateTime.Parse(date);
+					Solver slvr = new Solver(_db);
+
+					model.Result = slvr.SolveOne(owner, parsedDate);
+					model.Owner = owner;
+					model.Date = parsedDate;
+					model.IsRes = true;
+				}
+			}
+			catch (NullReferenceException e)
+			{
+				_logger.LogError(e.ToString());
+				model.IsError = true;
+				model.ErrMessage = "Выбранная дата отсутствует";
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.ToString());
+				model.IsError = true;
+				model.ErrMessage = "Внутренняя ошибка";
+			}
+
+			model.Portfolios = _db.PortfolioRepository.GetAll();
+			return View(model);
+		}
+
+		// Second assignment solver
+		public IActionResult Second(string[] stockNames)
+		{
+			SecondModel model = new SecondModel();
+			try
+			{
+				if (stockNames.Length > 0)
+				{
+					Solver slvr = new Solver(_db);
+					model.Result = slvr.SolveTwo(stockNames.ToList());
+					model.IsRes = true;
+				}
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.ToString());
+				model.IsError = true;
+				model.ErrMessage = "Внутренняя ошибка";
+			}
+		
+			model.Stocks = _db.StockRepository.GetAll();
+			return View(model);
+		}
+		
+		// Third assignment solver
+		public IActionResult Third(string owner, string dateFrom, string dateTo)
+		{
+			ThirdModel model = new ThirdModel();
+		
+			try
+			{
+				if (!String.IsNullOrWhiteSpace(owner) && !String.IsNullOrWhiteSpace(dateFrom) &&
+				    !String.IsNullOrWhiteSpace(dateTo))
+				{
+					DateTime dFrom = DateTime.Parse(dateFrom);
+					DateTime dTo = DateTime.Parse(dateTo);
+					Solver slvr = new Solver(_db);
+		
+					model.Result = slvr.SolveThree(owner, dFrom, dTo);
+					model.Owner = owner;
+					model.DateFrom = dFrom;
+					model.DateTo = dTo;
+					model.IsRes = true;
+				}
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e.ToString());
+				model.IsError = true;
+				model.ErrMessage = "Внутренняя ошибка";
+			}
+		
+			model.Portfolios = _db.PortfolioRepository.GetAll();
+			return View(model);
+		}
+	}
+}
